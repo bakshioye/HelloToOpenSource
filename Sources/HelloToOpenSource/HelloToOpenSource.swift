ea76f368c12a78b76d9d4af45882e76fae057d26
Shubham Bakshi <shubham.bakshi@dista.ai>
2024-11-13 18:18:57 +0530

    Initial commit

--- Sources/HelloToOpenSource/HelloToOpenSource.swift
+++ Sources/HelloToOpenSource/HelloToOpenSource.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct HelloOpenSource {
+    
+    public func greet() {
+        print("Hello from Open Source!")
+    }
+    
+}
